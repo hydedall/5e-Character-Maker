2975476fc3efa480a308ad6767b49217f49e9f6e
hydedall <hyde.dallas1@gmail.com>
2022-09-12 09:30:03 -0400

    server

---
+++ server.js
@@ -0,0 +1,39 @@
+// DEPENDENCIES
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
+// DATABASE CONFIGURATION
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Database Connection Error/Success
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// MIDDLEWARE  & BODY PARSER
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+//ROUTES
+
+//I
+//N
+//D
+//U
+//C
+//E
+//S
+
+// LISTENER
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`The serer is listening on port: ${PORT}`)
+})
