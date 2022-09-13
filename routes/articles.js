a40300feefb3d5c2714920289f0db21a9fd67153
hydedall <hyde.dallas1@gmail.com>
2022-09-13 15:30:55 -0400

    new

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.send('articles/new')
+    res.render('articles/new')
+})
+
+router.post('/', (req, res) => {
+    
 })
 
 module.exports = router
